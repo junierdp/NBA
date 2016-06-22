35290ccc140eadf181feacec9d0185b245e878be
Junier <onieldp@gmail.com>
2016-06-22 17:53:42 -0400

    modified

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'localhost:3307',
+	'username' => 'root',
+	'password' => 'oniel2908',
+	'database' => 'NBA',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
